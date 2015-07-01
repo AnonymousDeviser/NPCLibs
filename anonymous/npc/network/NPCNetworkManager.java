5400af7f21427626b79d03893c923f71b223e5ae
Anonymous Dev <AnonymousDeviser@users.noreply.github.com>
2015-07-01 15:21:50 -0300

    Create NPCNetworkManager.java

---
+++ anonymous/npc/network/NPCNetworkManager.java
@@ -0,0 +1,23 @@
+package anonymous.npc.network;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.server.v1_7_R4.NetworkManager;
+
+public class NPCNetworkManager extends NetworkManager {
+	
+	public NPCNetworkManager() throws Exception {
+		super(false);
+		swapFields();
+	}
+		  
+	protected void swapFields() throws Exception {
+		Field channel = NetworkManager.class.getDeclaredField("m");
+		Field address = NetworkManager.class.getDeclaredField("n");
+		channel.setAccessible(true);
+	    address.setAccessible(true);
+	    channel.set(this, new NullChannel(null));
+	    address.set(this, null);
+	}
+
+}
